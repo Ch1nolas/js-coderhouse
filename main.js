d304ad2eba6ed88e994f3f965b709fecba9f3034
Chinolas <nmartinez252524342@gmail.com>
2022-12-31 00:19:58 -0300

    Nuevos archivos

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+
+let nombre = prompt ("Ingrese su nombre");
+if (nombre == "nico") {
+    console.log ("Hola Nico");
+}   else {
+    console.log ("No eres nico");
+}
+*/
+
+for (let i=0; i<10; i++) {
+    console.log ("Hola mundo");
+}
